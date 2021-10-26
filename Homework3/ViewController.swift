f4f6f08adf66f8d140e4cfee80dfb7f64f1f2bef
Boiko Konstantin <92636892+tipachel88@users.noreply.github.com>
2021-10-26 17:30:39 +0300

    Вёрстка

--- Homework3/ViewController.swift
+++ Homework3/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  
+    }
+    @IBAction func loginButtonPressed() {
     }
 
-
+    @IBAction func forgotUsername() {
+    }
+   
+    @IBAction func forgotPassword() {
+    }
+    
+    
 }
 
