0415f32e369dff86873e59e4c84bc517cc390e7a
Boiko Konstantin <92636892+tipachel88@users.noreply.github.com>
2021-10-26 21:33:52 +0300

    Без дополнительного задания

--- Homework3/AccountViewController.swift
+++ Homework3/AccountViewController.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 class AccountViewController: UIViewController {
-
+    
+    @IBOutlet var greetings: UILabel!
+    var username:String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        greetings.text = "Congratulations, \(username ?? "")!"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logOutButtonPressed() {
+        
     }
-    */
-
 }
--- Homework3/ViewController.swift
+++ Homework3/ViewController.swift
@@ -8,24 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let login = "Shinji"
+    let password = "ikari"
+    
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-  
     }
+    
     @IBAction func loginButtonPressed() {
+        
+        if userNameTF.text == "" && passwordTF.text == ""{
+            showAlert(title: "Acces denied", message:"Fill the fields ")
+        }else if userNameTF.text != login || passwordTF.text != password {
+            showAlert(title: "Accec denied", message: "User name of password is incorrect!")
+            
+        }
     }
-
+    
     @IBAction func forgotUsername() {
+        showAlert(title: "Forgot your name", message: "Your name is \(login)")
     }
-   
+    
+    
     @IBAction func forgotPassword() {
+        showAlert(title: "Forgot your password", message: "Your password is \(password) ")
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let accountVC = segue.destination as? AccountViewController else { return }
+        
+        accountVC.username = userNameTF.text
+    }
     
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        passwordTF.text = ""
+        
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with:event)
+    }
 }
 
+
+
+
+extension ViewController {
+    private func showAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default){ _ in
+            
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
